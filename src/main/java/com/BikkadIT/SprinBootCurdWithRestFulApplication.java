4a448a88237e6ff77865af4aa1eb09f4a56025f1
VishakhaBairamwar <bairamwarvishakha@gmail.com>
2022-08-12 17:02:20 +0530

    SBCWR-02:Create Entity Class

---
+++ src/main/java/com/BikkadIT/SprinBootCurdWithRestFulApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootCurdWithRestFulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootCurdWithRestFulApplication.class, args);
+	}
+
+}
